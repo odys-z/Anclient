35c6253378981d0323add0a629554d1a7cf4227c
odysz <odysseusj@163.com>
2019-02-24 10:55:37 +0800

    -

--- js/lib/view/vue/jcomponents.js
+++ js/lib/view/vue/jcomponents.js
@@ -5,36 +5,10 @@
 	The component can not been imported as named component. See
 	https://github.com/vuejs/vue-loader/issues/1234
 	But user shouldn't import every UI wedget one by one. So we provided them in a pack.
-<template>
-	<div>
-	</div>
-</template>
-<script>
  */
-  // import Vue from 'vue/dist/vue.js'
   import Listoolbar from './mixins/listoolbar.vue'
   import Semantable from './table/semantable.vue'
   import SidebarMenu from './menu/sidebarMenu.vue'
 
-  // Vue.component('SidebarMenu', SidebarMenu);
-  // Vue.component('sidebar-menu', SidebarMenu);
-
-  // export default {
-	// name: 'jcomponents',
-	// components: {
-	// 	Listoolbar, Semantable,
-	// },
-	// data() {
-	// 	return {};
-	// }
-	// 		// register globally
-	// 		// https://vuejs.org/v2/guide/components-registration.html#Local-Registration
-	// 		// https://github.com/chrisvfritz/vue-enterprise-boilerplate/blob/master/src/components/_globals.js
-	// 		// Vue.component(componentName, componentConfig.default || componentConfig);
-	// 		// Vue.component('SidebarMenu', SidebarMenu);
-  // }
-
+  //  way of vuent
   export {default as SidebarMenu} from './menu/sidebarMenu.vue'
-/*
-</script>
-*/
