3edd5c139e540a41d02b39089eb25541e51c4dfa
odys zhou <odysseusj@163.com>
2019-03-30 16:39:32 +0800

    io.odysz.semantic.ext.DatasetReqTest.testToFromJson(DatasetReqTest.java:53)

--- java/eclipse-workspace/jclient.java/src/main/java/io/odysz/jclient/cheapflow/CheapClient.java
+++ java/eclipse-workspace/jclient.java/src/main/java/io/odysz/jclient/cheapflow/CheapClient.java
@@ -3,7 +3,6 @@ package io.odysz.jclient.cheapflow;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import io.odysz.common.Utils;
 import io.odysz.jclient.SessionClient;
 import io.odysz.jsample.cheap.CheapReq;
 import io.odysz.jsample.protocol.Samport;
@@ -11,6 +10,7 @@ import io.odysz.semantic.jprotocol.JBody;
 import io.odysz.semantic.jprotocol.JHeader;
 import io.odysz.semantic.jprotocol.JMessage;
 import io.odysz.semantic.jprotocol.JProtocol.SCallback;
+import io.odysz.semantics.SemanticObject;
 import io.odysz.semantics.x.SemanticException;
 import io.odysz.sworkflow.EnginDesign.Req;
 
@@ -28,8 +28,18 @@ public class CheapClient {
 		this.ssclient = ssclient;
 	}
 
-	public void start(String wfid, SCallback onOk) throws SemanticException, IOException, SQLException {
-		CheapReq req = new CheapReq(null, "jserv-sample")
+	/**Send a starting request to CheapServ.
+	 * @param wfid
+	 * @param act client action for logging user's finger print at server side, typically got with
+	 * <pre>JHeader.usrAct("CheapClient Test", "start", t,
+		"test jclient.java starting wf " + wfId);</pre>
+	 * @param onOk
+	 * @throws SemanticException
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public void start(String wfid, String[] act, SCallback onOk) throws SemanticException, IOException, SQLException {
+		CheapReq req = new CheapReq(null)
 				.req(Req.start).wftype(wfid)
 				.nodeDesc("Desc: bla")
 				.childTabl("task_details")
@@ -37,8 +47,6 @@ public class CheapClient {
 				.newChildInstRow().childInsert("remarks", "client detail - 02");
 
 		String t = Req.start.name();
-		String[] act = JHeader.usrAct("CheapClient Test", "start", t,
-				"test jclient.java starting wf " + wfId);
 
 		JMessage<? extends JBody> jmsg = ssclient.userReq(t, Samport.cheapflow, act, req);
 		jmsg.header(ssclient.header());
@@ -46,18 +54,64 @@ public class CheapClient {
 		ssclient.console(jmsg);
 		
     	ssclient.commit(jmsg, (code, data) -> {
-			String e = (String) data.get("evt");
-			Utils.logi(e);
+			SemanticObject e = (SemanticObject) data.get("evt");
+			onOk.onCallback("ok", e);
     	});
 
 	}
 
-	public void step(String wfid, String cmd, SCallback onOk) {
+	/**Send a stepping command request to CheapServ.
+	 * @param wfid
+	 * @param cmd
+	 * @param act client action for logging user's finger print at server side, typically got with
+	 * <pre>JHeader.usrAct("CheapClient Test", "read", t,
+		"test jclient.java querying rights " + wfId);</pre>
+	 * @param onOk
+	 * @throws SQLException
+	 * @throws SemanticException
+	 * @throws IOException
+	 */
+	public void step(String wfid, String cmd, String[] act, SCallback onOk)
+			throws SQLException, SemanticException, IOException {
+		CheapReq req = new CheapReq(null)
+				.reqCmd(cmd)
+				.wftype(wfid);
+
+		String t = Req.cmd.name();
+
+		JMessage<? extends JBody> jmsg = ssclient.userReq(t, Samport.cheapflow, act, req);
+		jmsg.header(ssclient.header());
+
+		ssclient.console(jmsg);
 		
+    	ssclient.commit(jmsg, (code, data) -> {
+			SemanticObject e = (SemanticObject) data.get("evt");
+			onOk.onCallback("ok", e);
+    	});
+
 	}
 
-	public void step3(String wfid, String cmd, SCallback onOk) {
+	public void rights(String wfid, String taskId, String currentNodeId, String usrId, SCallback onOk)
+			throws SQLException, SemanticException, IOException {
+		CheapReq req = new CheapReq(null)
+				.cmdsRight(currentNodeId, usrId, taskId)
+				.wftype(wfid);
+
+		String t = Req.cmdsRight.name();
+		String[] act = JHeader.usrAct("", "read", t,
+				"jclient.java querying rights " + wfId);
+
+		JMessage<? extends JBody> jmsg = ssclient.userReq(t, Samport.cheapflow, act, req);
+		jmsg.header(ssclient.header());
+
+		ssclient.console(jmsg);
 		
+    	ssclient.commit(jmsg, (code, data) -> {
+			SemanticObject e = (SemanticObject) data.get("rights");
+			onOk.onCallback("ok", e);
+    	});
+
+	
 	}
 
 }
--- java/eclipse-workspace/jclient.java/src/test/java/io/odysz/cheapflow/CheapClientTest.java
+++ java/eclipse-workspace/jclient.java/src/test/java/io/odysz/cheapflow/CheapClientTest.java
@@ -10,7 +10,11 @@ import io.odysz.common.Utils;
 import io.odysz.jclient.Clients;
 import io.odysz.jclient.SessionClient;
 import io.odysz.jclient.cheapflow.CheapClient;
+import io.odysz.semantic.jprotocol.JHeader;
+import io.odysz.semantics.SemanticObject;
 import io.odysz.semantics.x.SemanticException;
+import io.odysz.sworkflow.CheapEvent;
+import io.odysz.transact.x.TransException;
 
 class CheapClientTest {
 	static final String jserv = "http://localhost:8080/semantic.jserv";
@@ -29,18 +33,42 @@ class CheapClientTest {
     		SessionClient ssc = Clients.login("admin", pswd);
     		
     		CheapClient cheap = new CheapClient(ssc);
-    		cheap.start(wfId, (c, dat) -> {
+    		String[] act = JHeader.usrAct("CheapClient Test", "start", "cheap",
+				"test jclient.java starting wf " + wfId);
+    		cheap.start(wfId, act, (c, dat) -> {
     			// fail("Not yet implemented");
-    		});
-    		
+				try {
+					// concurrency 1: ask current rights
+					CheapEvent evt = new CheapEvent(dat);
+					cheap.rights(wfId, evt.taskId(), evt.currentNodeId(), "admin", (c0, dat0) -> {
+						((SemanticObject)dat0).print(System.out);
+					});
 
-    		cheap.step(wfId, cmdA, (c, dt) -> {
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// TODO let's do after js
+					// concurrency 2: step the started task
+//					String[] atc = JHeader.usrAct("CheapClient Test", "step", "cheap",
+//							"test jclient.java stepping wf " + wfId);
+//					cheap.step(wfId, cmdA, atc, (c1, dt) -> {
+//					});
     			
+				} catch (TransException e) {
+					e.printStackTrace();
+				}
     		});
     		
-    		cheap.step3(wfId, cmd3, (c, dt) -> {
-    			
-    		});
+//    		cheap.step(wfId, cmd3, act, (c, dt) -> {
+//    			
+//    		});
+
     	} catch (IOException io) {
     		Utils.warn("loging failed: %s", io.getMessage());
     	}
