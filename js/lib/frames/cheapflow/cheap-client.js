1ca4119a91b4accd28036de9ff5814f7375c29fd
odys zhou <odysseusj@163.com>
2019-06-13 18:13:29 +0800

    0

--- js/lib/frames/cheapflow/cheap-client.js
+++ js/lib/frames/cheapflow/cheap-client.js
@@ -1,159 +1,102 @@
 
-import $ from 'jquery';
-import {Protocol, JMessage, JHeader, QueryReq, UserReq} from '../../protocol.js';
+/* */
 
+function CheapClient (ssclient, cheaport) {
+	this.connId = null;
 
-/**See semantic.workflow/io.odysz.sworkflow.EnginDesign.Req */
-const chpEnumReq = {
-	heartbeat: "ping",
-	/** load workflow list */
-	load: "load",
-	/** load a node's commnads */
-	nodeCmds: 'nodeCmds',
-	TgetDef: "get-def",
-	findRoute: "findroute",
-	cmdsRight: "right",
-	/** client use this to ask plausible operation using 't' */
-	Ttest: "test",
-	start: "start",
-	/**request stepping according to cmds configured in oz_wfcmds */
-	cmd: "cmd",
-	close: "close",
-	timeout: "timeout",
+	if (typeof cheaport === 'string')
+		this.cheaport = cheaport;
+	else this.cheaport = jvue.CheapReq.port;
+
+	this.jclient = ssclient;
+	this.cheapReq = jvue.chpEnumReq;
+
+	this.conn = function (conn) {
+		this.connId = conn;
+	};
+
+	/**Load a task's workflow nodes joined with instances.
+	 * @param {string} wfId
+	 * @param {object} opts<br>
+	 * opts.taskId: task id<br>
+	 */
+	this.loadFlow = function (wfId, opts) {
+		// this.cmdArgs = [wfId, taskId];
+		var cheapReq = new jvue.CheapReq(wfId)
+						.a(this.cheapReq.load)
+						.taskId(opts.taskId);
+		var jmsg = this.jclient.userReq(this.connId, engports.cheapflow, cheapReq);
+		this.jclient.commit(jmsg, opts.onok, function (c, d) {
+			if (typeof opts.onerror === 'function')
+				opts.onerror(c, d);
+			else EasyMsger.error(c, d);
+		});
+		return this;
+	}
+
+	/**Load commands
+	 * @param {string} wfId
+	 * @param {object} opts<br>
+	 * opts.taskId: task id<br>
+	 * opts.nodeId: node Id<br>
+	 * opts.usrId: user Id<br>
+	 */
+	this.loadCmds = function (wfId, opts) {
+		if (typeof wfId !== 'string' || typeof opts.nodeId !== 'string') {
+			console.error('Parameters invalid: wfId, opts.nodeId',
+				wfId, opts);
+			return;
+		}
+
+		var cheapReq = new jvue.CheapReq(wfId)
+						.a(this.cheapReq.nodeCmds)
+						.nodeId(opts.nodeId)
+						.usrId(opts.usrId)
+						.taskId(opts.taskId); // optional - not used
+		var jmsg = this.jclient.userReq(this.connId, engports.cheapflow, cheapReq);
+		this.jclient.commit(jmsg, opts.onok, function (c, d) {
+			if (typeof opts.onerror === 'function')
+				opts.onerror(c, d);
+			else EasyMsger.error(c, d);
+		});
+		return this;
+	}
+
+	/**Post a workflow starting request.
+	 * @param {CheapReq} cheapReq the request body
+	 * @param {object} opts options<br>
+	 * opts.taskId {string}: (Optional) task id (bussiness form record id)<br>
+	 * opts.onok {function}: on ok callback<br>
+	 * @return {CheapClient} this
+	 */
+	this.start = function (cheapReq, opts) {
+		cheapReq.a(this.cheapReq.start)	// this.cheapReq is the req enum
+				.taskId(opts.taskId)
+		var jmsg = this.jclient.userReq(this.connId, engports.cheapflow, cheapReq);
+		this.jclient.commit(jmsg, opts.onok, function (c, d) {
+			if (typeof opts.onerror === 'function')
+				opts.onerror(c, d);
+			else if (c === jvue.CheapCode.err_rights)
+				EasyMsger.alert(EasyMsger.m.cheap_no_rights);
+			else
+				EasyMsger.error(c, d);
+		});
+		return this;
+	}
+
+	this.step = function (cheapBody, opts) {
+		cheapBody.a(this.cheapReq.cmd);
+		var jmsg = this.jclient.userReq(this.connId, engports.cheapflow, cheapBody);
+		this.jclient.commit(jmsg, opts.onok, opts.onerror);
+	}
+
+	this.rights = function (wfid, taskId, currentNodeId, usrId, opts) {
+		var req = new jvue.CheapReq(this.jclient, this.cheaport)
+						.a(this.cheapReq.load)
+						.arg('wfId', wfid)
+						.arg('taskId', taskId)
+						.arg('currentNode', currentNodeId)
+						.arg('usrId', usrId);
+		this.jclient.commit(req, opts.onok);
+	}
 }
-
-/** @See io.odysz.sworkflow.CheapException */
-const CheapCode = {
-	ok: 'ok',
-	err: 'wf_err',
-	err_rights: 'wf_err_rights',
-	err_competation: 'wf_err_competing',
-}
-
-///////////////// io.odysz.sworkflow ///////////////////////////////////////////
-class CheapReq {
-	// a (req.name)
-	// protected String wftype;
-	// protected String[] cmdArgs;
-	// protected String ndescpt;
-	// protected String childTbl;
-	// protected ArrayList<String[]> taskNvs;
-	// protected ArrayList<ArrayList<String[]>> childInserts
-
-	constructor (wftype, port) {
-		this.wftype = wftype;
-		if (port === undefined)
-			this.port = 'cheapflow.sample';
-		else this.port = port;
-	}
-
-	/**set a.<br>
-	 * a() can only been called once.
-	 * @param {string} a
-	 * @return {UserReq} this */
-	a(a) {
-		this.a = a;
-		return this;
-	}
-
-	cmd(c) {
-		this.cmd = c;
-		return this;
-	}
-
-	arg(n, v) {
-		if (v !== undefined) { // getting
-			if (this.cmdArgs === undefined)
-				this.cmdArgs = {};
-			// this.cmdArgs = Object.assign(this.cmdArgs, {n, v});
-			this.cmdArgs[n] = v;
-			return this;
-		}
-		else {
-			if (this.cmdArgs === undefined) return;
-			// for (var ix = 0; ix < this.cmdArgs.length; ix++) {
-			// 	if (this.cmdArgs[ix][0] === n)
-			// 		return this.cmdArgs[ix][1];
-			// }
-			return this.cmdArgs[n];
-		}
-	}
-
-	nodeDesc (descpt) {
-		this.ndescpt = descpt;
-		return this;
-	}
-
-	taskNv (n, v) {
-		if (this.taskNvs === undefined || this.taskNvs === null)
-			this.taskNvs = [];
-		this.taskNvs.push([n, v]);
-		return this;
-	}
-
-	taskRow (row) {
-		if (Array.isArray(row)) {
-			for (var ix = 0; ix < row.length && Array.isArray(row[ix]); ix++) {
-				this.taskNv(row[ix][0], row[ix][1]);
-			}
-		}
-		else {
-			console.error('CheapReq.taskRow(row) argument format:\n',
-						'[[n, v], ...]\n',
-						row);
-		}
-		return this;
-	}
-
-	taskId (tid) {
-		return this.arg('taskId', tid);
-	}
-
-	nodeId (nid) {
-		return this.arg('nodeId', nid);
-	}
-
-	instId (iid) {
-		return this.arg('instId', iid);
-	}
-
-	usrId (uid) {
-		return this.arg('usrId', uid);
-	}
-
-	/**Add a post update request to the transact batch operations
-	 * @param {UpdateReq | InsertReq} jbody post request
-	 * @return {CheapReq} this*/
-	post (jbody) {
-		// FIXME there is no such thing in CheapReq.java
-		if (this.childInserts === undefined || this.childInserts === null)
-			this.childInserts = [];
-		else this.childInserts.push(jbody);
-		return this;
-	}
-
-	// FIXME
-	childInsert (n, v) {
-		// FIXME call post() instead
-		this.childInserts[this.childInserts.length - 1].push([n, v]);
-		return this;
-	}
-
-	// req (r) {
-	// 	this.a = r;
-	// 	return this;
-	// }
-
-	// use ssClient.usrCmd() instead
-	// reqCmd (cmd) {
-	// 	this.cmdArgs = [cmd];
-	// 	return req(chpEnumReq.cmd);
-	// }
-
-	// cmdsRight (nodeId, usrId, taskId) {
-	// 	this.cmdArgs = [nodeId, usrId, taskId];
-	// 	return req(chpEnumReq.cmdsRight);
-	// }
-}
-
-export {CheapReq, chpEnumReq, CheapCode};
