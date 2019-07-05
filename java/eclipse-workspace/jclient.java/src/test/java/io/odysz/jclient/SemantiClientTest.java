4120676b367db0a3f761b9cd946849b020f8122c
odysz <odysseusj@163.com>
2019-07-05 20:48:07 +0800

    1

--- java/eclipse-workspace/jclient.java/src/test/java/io/odysz/jclient/SemantiClientTest.java
+++ java/eclipse-workspace/jclient.java/src/test/java/io/odysz/jclient/SemantiClientTest.java
@@ -9,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
