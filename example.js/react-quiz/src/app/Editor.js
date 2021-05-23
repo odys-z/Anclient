cfb9ca7214859dd0e748f38eaf61326978736766
odysz <odysseusj@163.com>
2021-05-23 17:48:51 +0800

    ..
    g

--- example.js/react-quiz/src/app/Editor.js
+++ example.js/react-quiz/src/app/Editor.js
@@ -7,19 +7,30 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Add from '@material-ui/icons/Add';
 import DraftsIcon from '@material-ui/icons/Drafts';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
 import SendIcon from '@material-ui/icons/Send';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import StarBorder from '@material-ui/icons/StarBorder';
+import Sms from '@material-ui/icons/Sms';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 
+var quid = -1;
+
+const Question = {
+	single: 0,
+	multiple: 1
+}
 
 class Editor extends React.Component {
+	static getQx() {
+		return ++quid;
+	}
 
 	classes = makeStyles({
 		root: {
@@ -27,12 +38,17 @@ class Editor extends React.Component {
 			maxWidth: 360,
 		},
 		nested: {
+			'background-color': 'red'
 		},
 	});
 
     state = {
-        questions: [['id0', 'q0', 'a b c d'], ['id1', 'q1', '1 2 3 4'], ['id2', 'q2', 'I II III IV']],
+		openHead: true,
+		qtitle: '',
+		qremraks: '',
+        questions: [], // id, questions, answers, type, correct index
         currentqx: -1,
+		autosave: true,
     };
 
 	constructor(props) {
@@ -41,6 +57,8 @@ class Editor extends React.Component {
 		this.editQuestion = this.editQuestion.bind(this);
 		this.editAnswer = this.editAnswer.bind(this);
 		this.onAdd = this.onAdd.bind(this);
+		this.onSave = this.onSave.bind(this);
+		this.onCheckSingle = this.onCheckSingle.bind(this);
 	}
 
 	handleClick(e) {
@@ -52,25 +70,39 @@ class Editor extends React.Component {
 
 	editQuestion(e) {
 		console.log(e.target.value);
-		let qx = this.state.questions.currentqx;
+		let qx = this.state.currentqx;
 		let questions = this.state.questions.slice();
-		let answers = questions[qx] && questions[qx].length > 1 ? questions[qx][1] : "";
-		questions[qx] = [e.target.value, answers];
+		questions[qx][1] = e.target.value;
 		this.setState({questions});
 	}
 
 	editAnswer(e) {
 		console.log(e.target.value);
-		let qx = this.state.questions.currentqx;
+		let qx = this.state.currentqx;
 		let questions = this.state.questions.slice();
-		let qtext = questions[qx] && questions[qx].length > 0 ? questions[qx][0] : "";
-		questions[qx] = [qtext, e.target.value];
+		questions[qx][2] = e.target.value;
 		this.setState({questions});
 	}
 
 	onAdd(e) {
-		this.state.questions.push('');
-		this.setState({currentqx: ++this.state.currentqx});
+		let qx = Editor.getQx();
+		let questions = this.state.questions.splice(0);
+		// let tp = e.currentTarget.children.filter((e, x) => e.name === 'qtype');
+		questions.push(['id'+ qx, 'Question ' + qx, 'A.\nB.\nC.\nD.', Question.single]);
+		this.setState({
+			questions,
+			currentqx: qx,
+			open: qx});
+	}
+
+	onCheckSingle(e) {
+		let questions = this.state.questions;
+		questions[this.state.currentqx][3] = e.target.checked ? Question.single : Question.multiple;
+		this.setState({questions});
+	}
+
+	onSave(e) {
+		console.log(this.state.questions);
 	}
 
 	items(start, end = -1) {
@@ -81,71 +113,35 @@ class Editor extends React.Component {
 
 		return this.state.questions.map((q, x) => (
 			  <div key={this.state.questions[x][0]}>
-				<ListItem button qx={x} onClick={this.handleClick}>
-					<ListItemIcon><DraftsIcon /></ListItemIcon>
+				<ListItem button qx={x} onClick={this.handleClick} color='secondary'>
+					<ListItemIcon><Sms /></ListItemIcon>
 					<ListItemText primary={this.state.questions[x][1]} />
 				</ListItem>
 				<Collapse in={this.state.currentqx == x} timeout="auto" >
-					<List component="div" disablePadding>
+					<List component="div">
 					  <ListItem button className={ this.classes.nested }>
 					    <ListItemIcon><StarBorder /></ListItemIcon>
-					    <ListItemText primary="Option A" />
-					    <ListItemText primary="Option B" />
-					    <ListItemText primary="Option C" />
+					    <ListItemText primary="Option..." />
 					    <FormControlLabel
-					        control={<Checkbox checked={this.state.check0}
+					        control={<Checkbox checked={this.state.questions[x][3] === Question.single}
+											   onClick={this.onCheckSingle}
 					                           name="chk0" color="primary" />}
-					        label="Primary"/>
+					        label="Single Answer"/>
 					  </ListItem>
 					</List>
 
 					<TextField id="qtext" label="Question"
 					  variant="outlined" color="primary"
-					  multiline fullWidth={true}
+					  multiline fullWidth={true} value={this.state.questions[x][1]}
 					  onChange={this.editQuestion} />
 
 					<TextField id="answers" label="Answers"
 					  variant="outlined" color="secondary"
-					  multiline fullWidth={true}
+					  multiline fullWidth={true} value={this.state.questions[x][2]}
 					  onChange={this.editAnswer} />
 				</Collapse>
 			  </div>
 			));
-	}
-
-	editem(qx) {
-		return
-		(<>
-			<ListItem button onClick={this.handleClick}>
-				<ListItemIcon><InboxIcon /></ListItemIcon>
-				<ListItemText primary="Inbox" />
-					{open ? <ExpandLess /> : <ExpandMore />}
-			</ListItem>
-			<Collapse in={this.state.open} timeout="auto" >
-				<List component="div" disablePadding>
-				  <ListItem button className={ this.classes.nested }>
-				    <ListItemIcon><StarBorder /></ListItemIcon>
-				    <ListItemText primary="Option A" />
-				    <ListItemText primary="Option B" />
-				    <ListItemText primary="Option C" />
-				    <FormControlLabel
-				        control={<Checkbox checked={this.state.check0}
-				                           name="chk0" color="primary" />}
-				        label="Primary"/>
-				  </ListItem>
-				</List>
-
-				<TextField id="qtext" label="Question"
-				  variant="outlined" color="primary"
-				  multiline fullWidth={true}
-				  onChange={this.editQuestion} />
-
-				<TextField id="answers" label="Answers"
-				  variant="outlined" color="secondary"
-				  multiline fullWidth={true}
-				  onChange={this.editAnswer} />
-			</Collapse>
-		</>);
 	}
 
 	render() {
@@ -159,19 +155,49 @@ class Editor extends React.Component {
 				</ListSubheader>
 			}
 			className={ this.classes.root } >
-			<ListItem button>
+			<ListItem button onClick={e => this.setState({openHead: !this.state.openHead})}>
 				<ListItemIcon><SendIcon /></ListItemIcon>
-				<ListItemText primary="Add New" onClick={this.onAdd} />
+				<ListItemText primary="Quiz Head" />
 			</ListItem>
-			<ListItem button>
-				<ListItemIcon><DraftsIcon /></ListItemIcon>
-				<ListItemText primary="Drafts" />
-			</ListItem>
+			<Collapse in={this.state.openHead} timeout="auto" >
+				<List component="div">
+				  <ListItem button className={ this.classes.nested }>
+				    <ListItemIcon><StarBorder /></ListItemIcon>
+				    <ListItemText primary="TODO Options ..." />
+				    <FormControlLabel
+				        control={<Checkbox checked={this.state.check0}
+				                           name="qtype" color="primary" />}
+				        label="Check Each Answer"/>
+				  </ListItem>
+				</List>
+
+				<TextField id="qtitle" label="Title"
+				  variant="outlined" color="primary"
+				  multiline fullWidth={true}
+				  value={this.state.quizTitle} />
+
+				<TextField id="qremarks" label="Remarks"
+				  variant="outlined" color="secondary"
+				  multiline fullWidth={true}
+				  value={this.state.quizRemarks} />
+			</Collapse>
+
 			{this.items(0, this.state.currentqx)}
+
+			<ListItem button>
+				<ListItemIcon onClick={this.onAdd} ><Add /></ListItemIcon>
+				<ListItemText primary="Add New" onClick={this.onAdd} />
+
+			    <FormControlLabel
+			        control={<Checkbox checked={this.state.autosave}
+									   onClick={e => {this.setState({autosave:!this.state.autosave});} }
+			                           name="autosave" color="secondary" />}
+			        label="Auto Save"/>
+
+				<ListItemText primary="Save" onClick={this.onSave} color="secondary" />
+			</ListItem>
 		  </List>);
 	}
-			// {this.editem(this.state.currentqx)}
-			// {this.state.currentqx >= 0 ? this.items(this.state.currentqx + 1) : ''}
 }
 
 ReactDOM.render(<Editor />, document.querySelector('#editor'));
