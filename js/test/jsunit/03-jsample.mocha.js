89330f388b7aabec4444d54df8bcccbaf8413bdd
odys zhou <odysseusj@163.com>
2021-07-09 17:07:14 +0800

    extending dataset/menu...

--- js/test/jsunit/03-jsample.mocha.js
+++ js/test/jsunit/03-jsample.mocha.js
@@ -1,6 +1,24 @@
 import { expect, assert } from 'chai'
 import { Protocol, AnsonMsg, AnsonResp } from '../../lib/protocol.js'
 
+const dsResp = {
+	"type": "io.odysz.semantic.jprotocol.test.AnsonMsg",
+	"code": "ok", "opts": null,
+	"port": "dataset", "header": null,
+	"vestion": "1.0",
+	"body": [{"type": "io.odysz.semantic.ext.test.AnDatasetResp",
+			  "rs": [{  "type": "io.odysz.anson.AnsonResultset",
+			  			"stringFormats": null, "total": 0, "rowCnt": 2, "colCnt": 2,
+						"colnames": {"1": [1, "1"], "2": [2, "2"]},
+						"rowIdx": 0,
+						"results": [["0, 1", "0, 2"], ["1, 1", "1, 2"]]
+					}],
+			  "parent": "io.odysz.semantic.jprotocol.test.AnsonMsg",
+			  "a": null,
+			  "forest": null, "conn": null, "m": "", "map": null
+	}], "seq": 0
+}
+
 describe('case: [03.1 Jsample.menu]', () => {
 
 	it('Convert to menu.serv requests', () => {
@@ -19,7 +37,6 @@ describe('case: [03.1 Jsample.menu]', () => {
 		assert.equal(mr.data.props['title'], 'user-req', "6 ---");
 		assert.equal(mr.data.props.quizId, '000001', "7 ---");
 
-		debugger
 		let port = 'test1';
 		let jreq = new AnsonMsg({
 					port,
@@ -37,5 +54,10 @@ describe('case: [03.1 Jsample.menu]', () => {
 	});
 
 	it('Convert AnsonResp to menu', () => {
+		let rp = new AnsonMsg(dsResp);
+		assert.equal(rp.code, 'ok', "1 ---");
+		assert.equal(rp.port, 'dataset', "2 ---");
+
+		assert.equal(rp.Body().rs(0).type, "io.odysz.anson.AnsonResultset", "3 ---");
 	});
 })
