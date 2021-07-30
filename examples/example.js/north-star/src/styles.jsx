0547aaf46737a3a27d7c3e3473fa244e8f2551f9
odys zhou <odysseusj@163.com>
2021-07-30 10:30:35 +0800

    ..

--- examples/example.js/north-star/src/styles.jsx
+++ examples/example.js/north-star/src/styles.jsx
@@ -19,7 +19,7 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import StarBorder from '@material-ui/icons/StarBorder';
 
-export const JsampleIcons = {
+export const StarIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
