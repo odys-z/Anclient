abed9510db8995e0663c5e882315c5d62e26ffa7
odys zhou <odysseusj@163.com>
2021-07-30 10:30:54 +0800

    .

---
+++ examples/example.js/north-star/src/north-app.jsx
@@ -0,0 +1,147 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import {SessionClient, Protocol, L, Langstrs,
+	 Sys, SysComp, AnContext, AnError, AnReactExt } from 'anclient'
+
+// tests extents
+import { samports } from '../jsample.js'
+	import { Domain } from '../../../test/react/views/domain'
+	import { Roles } from './views/roles'
+	import { Orgs } from './views/orgs'
+	import { Users } from './views/users'
+	import { Indicatrees } from './views/indicatrees'
+	import { Northeme } from './styles'
+
+/** The application main.
+ * "North" stands for the guardian.
+ */
+class NorthApp extends React.Component {
+	state = {
+		anClient: undefined, // SessionClient
+		anReact: undefined,  // helper for React
+		hasError: false,
+		iportal: 'portal.html',
+		nextAction: undefined, // e.g. re-login
+
+		error: undefined,
+	};
+
+	/**Restor session from window.localStorage
+	 */
+	constructor(props) {
+		super(props);
+
+		this.state.samports = Object.assign(Protocol.Port, samports);
+		this.state.iportal = this.props.iportal;
+
+		this.onError = this.onError.bind(this);
+		this.onErrorClose = this.onErrorClose.bind(this);
+		this.logout = this.logout.bind(this);
+
+		// design: will load anclient from localStorage
+		this.state.error = {onError: this.onError, msg: ''};
+		this.state.anClient = new SessionClient();
+		this.state.anReact = new AnReactExt(this.state.anClient, this.state.error)
+								.extendPorts(samports);
+
+		// singleton error handler
+		if (!this.state.anClient || !this.state.anClient.ssInf) {
+			this.state = Object.assign(this.state, {
+				nextAction: 're-login',
+				hasError: true,
+				err: L('Creating session failed! Please re-login.')});
+		}
+
+		// extending CRUD pages
+		SysComp.extendLinks( [
+			{path: '/sys/domain/domain', comp: Domain},
+			{path: '/sys/role/roles', comp: Roles},
+			{path: '/sys/org/orgs', comp: Orgs},
+			{path: '/sys/user/users', comp: Users},
+			{path: '/north/indicators', comp: Indicatrees},
+		] );
+	}
+
+	onError(c, r) {
+		console.error(c, r);
+		// this.setState({hasError: !!c, nextAction: 're-login'});
+		this.setState({hasError: !!c,
+			nextAction: c === Protocol.exSession ? 're-login' : 'ignore'});
+	}
+
+	onErrorClose() {
+		if (this.state.nextAction === 're-login') {
+			this.state.nextAction = undefined;
+			this.logout();
+		}
+	}
+
+	/** For navigate to portal page */
+	logout() {
+		// leaving
+		this.state.anClient.logout(
+			() => {
+				if (this.props.iwindow)
+					this.props.iwindow.location = this.state.iportal;
+			},
+			(c, e) => {
+				// something wrong
+				console.warn('Logou failed', c, e)
+				if (this.state.anClient)
+        			localStorage.setItem(SessionClient.ssInfo, null);
+				if (this.props.iwindow)
+					this.props.iwindow.location = this.state.iportal;
+			});
+		this.state.anClient = undefined;
+	}
+
+	render() {
+	  return (
+		<MuiThemeProvider theme={Northeme }>
+			<AnContext.Provider value={{
+				// FIXME we should use a better way
+				// https://reactjs.org/docs/legacy-context.html#how-to-use-context
+				samports: this.state.samports, // FXIME or Protocol?
+				anReact: this.state.anReact,
+				pageOrigin: window ? window.origin : 'localhost',
+				servId: this.state.servId,
+				servs: this.props.servs,
+				jserv: this.state.jserv,
+				anClient: this.state.anClient,
+				hasError: this.state.hasError,
+				iparent: this.props.iparent,
+				iportal: this.props.iportal || 'portal.html',
+				error: this.state.error,
+			}} >
+				<Sys onLogout={this.logout}/>
+				{this.state.hasError && <AnError onClose={this.onErrorClose} fullScreen={false} />}
+			</AnContext.Provider>
+		</MuiThemeProvider>);
+	}
+
+	/**Try figure out serv root, then bind to html tag.
+	 * First try ./private.json/<serv-id>,
+	 * then  ./github.json/<serv-id>,
+	 * where serv-id = this.context.servId || host
+	 *
+	 * For test, have elem = undefined
+	 * @param {string} elem html element id, null for test
+	 * @param {object} [opts={}] serv id
+	 * @param {string} [opts.serv='host'] serv id
+	 * @param {string} [opts.iportal='portal.html'] page showed after logout
+	 */
+	static bindHtml(elem, opts = {}) {
+		let portal = opts.portal ? opts.portal : 'portal.html';
+		AnReactExt.bindDom(elem, opts, onJsonServ);
+
+		function onJsonServ(elem, json) {
+			let dom = document.getElementById(elem);
+			ReactDOM.render(<NorthApp servs={json} servId={opts.serv} iportal={portal} iwindow={window}/>, dom);
+		}
+	}
+}
+
+export {NorthApp};
