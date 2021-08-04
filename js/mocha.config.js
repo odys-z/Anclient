050741db6f8f9aa0e9873455e3c1460fbf2adc9a
odysz <odysseusj@163.com>
2021-08-04 22:16:46 +0800

    ..

--- js/mocha.config.js
+++ js/mocha.config.js
@@ -21,12 +21,14 @@ var config = {
 
   plugins: [ ],
 
+  resolve: {
+	extensions: ['*', '.js', '.jsx']
+  },
+
   module: {
 	rules: [
 		{   test: /\.jsx$/,
 			loader: 'babel-loader',
-			// yes this is needed as testing example.js/lib/protocol.quiz.js
-			// which in tur using ../examples/example.js/node_modules/anclient/lib/react/crud.jsx
 			// exclude: /node_modules/,
 			options: {
 			  presets: ['@babel/preset-react', '@babel/preset-env'] }
