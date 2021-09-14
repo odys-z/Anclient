710b3316e564ba3b1c2a28e4ac86b8123013fb62
odys zhou <odysseusj@163.com>
2021-09-14 10:50:40 +0800

    ..

--- examples/example.js/north-star/common/port.js
+++ examples/example.js/north-star/common/port.js
@@ -5,6 +5,6 @@ export const StarPorts = {
 	north: 'north.serv',
 	center: 'center.serv',
 	nchart: 'nchart.serv',
-	userstier: 'users.tier'
+	userstier: 'users.tier',
 	mykidstier: "mykids.tier"
 }
