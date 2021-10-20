ecf06c8093ae548943344af0d62d72c89294a73c
odys zhou <odysseusj@163.com>
2021-10-20 21:41:47 +0800

    ..

--- js/anreact/src/jsample/styles.jsx
+++ js/anreact/src/jsample/styles.jsx
@@ -27,33 +27,33 @@ import FormatLineSpacingIcon from '@material-ui/icons/FormatLineSpacing';
 import GridOnIcon from '@material-ui/icons/GridOn';
 
 export const JsampleIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    Close: forwardRef((props, ref) => <Close {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  Close: forwardRef((props, ref) => <Close {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
 
-    ItemCollapse: forwardRef((props, ref) => <FormatLineSpacingIcon {...props} ref={ref} />),
-    Worksheet: forwardRef((props, ref) => <GridOnIcon {...props} ref={ref} />),
+  ItemCollapse: forwardRef((props, ref) => <FormatLineSpacingIcon {...props} ref={ref} />),
+  Worksheet: forwardRef((props, ref) => <GridOnIcon {...props} ref={ref} />),
 
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-	ListAdd: forwardRef((props, ref) => <PlaylistAddIcon {...props} ref={ref} />),
-    Star: forwardRef((props, ref) => <StarBorder {...props} ref={ref} />),
-	Up: forwardRef((props, ref) => <ArrowUpwardIcon  {...props} ref={ref} />),
-	Down: forwardRef((props, ref) => <ArrowDownwardIcon {...props} ref={ref} />)
-  };
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+  ListAdd: forwardRef((props, ref) => <PlaylistAddIcon {...props} ref={ref} />),
+  Star: forwardRef((props, ref) => <StarBorder {...props} ref={ref} />),
+  Up: forwardRef((props, ref) => <ArrowUpwardIcon  {...props} ref={ref} />),
+  Down: forwardRef((props, ref) => <ArrowDownwardIcon {...props} ref={ref} />)
+};
 
 export const JsampleTheme = responsiveFontSizes(createTheme({
 	// for example
