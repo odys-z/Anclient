0c1762912f8c3d51e4ff5308c1bb7bb5d8706e36
odys zhou <odysseusj@163.com>
2021-11-02 10:42:06 +0800

    ..

---
+++ js/anreact/src/an-components.ts
@@ -0,0 +1,58 @@
+export * from './utils/consts';
+export * from './utils/langstr';
+export * from './utils/helpers';
+
+export * from './patch/react-portal-overlay';
+
+export * from './react/reactext.jsx';
+export * from './react/anreact.jsx';
+export * from './react/error.jsx';
+export * from './react/login.jsx';
+export * from './react/sys.jsx';
+export * from './react/crud.jsx';
+
+export * from './react/widgets/messagebox';
+export * from './react/widgets/dataset-combo';
+export * from './react/widgets/image-upload';
+export * from './react/widgets/file-upload';
+export * from './react/widgets/my-icon';
+export * from './react/widgets/my-info';
+export * from './react/widgets/query-form';
+export * from './react/widgets/query-form-st';
+export * from './react/widgets/table-list';
+export * from './react/widgets/tabs';
+export * from './react/widgets/tree';
+export * from './react/widgets/treegrid';
+export * from './react/widgets/ag-gridsheet';
+export * from './react/widgets/tree-editor';
+
+export * from './react/widgets/simple-form';
+export * from './react/widgets/t-record-form';
+export * from './react/widgets/relation-tree';
+
+import { JsampleIcons, JsampleTheme, jstyles } from './jsample/styles';
+import { Domain, DomainComp } from './jsample/views/domain';
+import { Orgs, OrgsComp } from './jsample/views/orgs';
+import { Roles, RolesComp } from './jsample/views/roles';
+import { RoleDetails, RoleDetailsComp } from './jsample/views/role-details';
+
+import { Userst, UserstComp, UsersTier, UserstReq } from './jsample/views/users';
+import { UserDetailst, UserDetailstComp } from './jsample/views/user-details';
+import { MyInfCard, MyInfCardComp } from './jsample/views/my-infcard';
+import { MyPswd, MyPswdComp } from './jsample/views/my-pswdcard';
+
+export const jsample = {
+	JsampleIcons, JsampleTheme, jstyles,
+	Domain, DomainComp, Orgs, OrgsComp,
+	Roles, RolesComp, RoleDetails, RoleDetailsComp,
+	MyInfCard, MyInfCardComp,
+	MyPswd, MyPswdComp,
+	Userst, UserstComp, UserDetailst, UserDetailstComp, UsersTier, UserstReq
+};
+
+import { uarr2Base64, dataOfurl, mimeOf, urlOfdata } from './utils/file-utils';
+import { regex } from './utils/regex';
+
+export const utils = {
+	uarr2Base64, dataOfurl, mimeOf, urlOfdata, regex
+}
