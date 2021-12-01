ef8e708c459e7d2367836baaea0a2d2677b5940d
Odys Zhou <odys.zhou@gmail.com>
2021-12-01 19:30:53 +0800

    relationship handling: evolution from wild to civalized

---
+++ examples/example.js/north-star/common/mui-helpers.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+
+import { L } from '@anclient/anreact';
+import { Tierec } from '@anclient/semantier-st';
+
+/**
+ * Formatting json string (can be parsed by JSON) into &gt;Typography/&lg;.
+ * @param extra extra string (no-sql), e.g. {msg: 'my message'} - deprecated way of arg's for using Typescript.
+ * @param rkey
+ * @param record the db record instance incase message comes with parameters.
+ */
+export function myMsgFromIssuer( extra: string, rkey: string, rec: Tierec ) {
+	let json = JSON.parse(extra);
+	return ( <>
+		<Typography variant={'body2'} >
+				{L('Msg from North:')}
+		</Typography>
+		<Card key={rkey}>
+			{json.msg}
+		</Card>
+	</>);
+}
