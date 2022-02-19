db957f549c1842069b771ca5b0a97a7d4a305f5c
Odys Zhou <odysseusj@163.com>
2022-02-19 00:54:55 -0500

    .

--- java/eclipse-workspace/anclient.jserv/src/io/odysz/jclient/tier/ErrorCtx.java
+++ java/eclipse-workspace/anclient.jserv/src/io/odysz/jclient/tier/ErrorCtx.java
@@ -2,7 +2,6 @@ package io.odysz.jclient.tier;
 
 import io.odysz.semantic.jprotocol.AnsonMsg.MsgCode;
 import io.odysz.semantic.jprotocol.AnsonResp;
-import io.odysz.semantics.x.SemanticException;
 
 /**This type is used for keeping error handling consist with React TS version, 
  * which is the error handler provided by React context provide. It's the optimized
@@ -15,12 +14,12 @@ public interface ErrorCtx {
 
 	public default String msg () { return ""; };
 
-	public default void onError(MsgCode code, AnsonResp obj) throws SemanticException {
-		throw new SemanticException(obj.msg());
+	public default void onError(MsgCode code, AnsonResp obj) {
+		// throw new SemanticException(obj.msg());
 	}
 
-	public default void onError(MsgCode code, String msg, Object ...args) throws SemanticException {
-		throw new SemanticException(msg, args);
+	public default void onError(MsgCode code, String msg, Object ...args) {
+		// throw new SemanticException(msg, args);
 	}
 
 	public default ErrorCtx setSignal(String signal) { return this; };
