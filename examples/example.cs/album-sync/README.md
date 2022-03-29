cba41a8f9b1c61d7c061b8e091e7220b45482459
Odys Zhou <odys.zhou@gmail.com>
2022-03-29 03:08:19 -0400

    start WPF explorer

---
+++ examples/example.cs/album-sync/README.md
@@ -0,0 +1,7 @@
+# Album Synchronizer for Windows
+
+# Credits
+
+[Mikependon, TreeViewFileExplorer](https://github.com/mikependon/RepoDB.Tutorials/tree/master/WPF/TreeViewFileExplorer)
+
+[Michael Pendon](https://medium.com/@mikependon/designing-a-wpf-treeview-file-explorer-565a3f13f6f2)
---
+++ examples/example.cs/album-sync/album-sync/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TreeViewFileExplorer;
+
+namespace TreeViewFileExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        MainWindow main; 
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void onLogin(object sender, RoutedEventArgs e)
+        {
+            main = new MainWindow();
+            //main.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //main.SourceInitialized += (s, a) => main.WindowState = WindowState.Maximized;
+            main.ShowDialog();
+        }
+    }
+}
