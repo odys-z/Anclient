ae904b8bfd7da11849bf9fe36732855a416ca9cc
Odys Zhou <odys.zhou@gmail.com>
2022-04-27 17:44:30 +0800

    seal: WPF

---
+++ csharp/anclient/MediaGallery/Gallery.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaGallery
+{
+    /// <summary>
+    /// Interaction logic for Gallery.xaml
+    /// </summary>
+    public partial class Gallery : UserControl
+    {
+        public List<object> Items { get; set; } 
+        public Gallery()
+        {
+            InitializeComponent();
+        }
+    }
+}
