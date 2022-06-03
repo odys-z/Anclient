86491d4229e68cd5962a4dbcb4f18f023eea546d
Odys Zhou <odys.zhou@gmail.com>
2022-06-03 10:58:11 +0800

    ..

--- js/anreact/src/react/widgets/spreadsheet.tsx
+++ js/anreact/src/react/widgets/spreadsheet.tsx
@@ -164,6 +164,9 @@ export class SpreadsheetReq extends UserReq {
 	}
 }
 
+export class SpreadsheetResp extends AnsonResp {
+	rec: SpreadsheetRec;
+}
 
 export class Spreadsheetier extends Semantier {
 	static reqfactory: (conds: PageInf, rec?: SpreadsheetRec) => SpreadsheetReq;
