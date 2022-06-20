cd4d5e44bb72ba7e043b091102468523dfa64e20
odys zhou <odysseusj@163.com>
2022-06-20 09:14:32 +0800

    .

--- examples/example.js/curriculums/views/center/course-readonly.tsx
+++ examples/example.js/curriculums/views/center/course-readonly.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 import { AnsonMsg, AnsonResp, PageInf } from '@anclient/semantier';
