4ecb80b923c89f945d17bcaa1f365f0aff2839cf
odys zhou <odysseusj@163.com>
2023-07-17 19:13:16 +0800

    fix doctree jserv

--- examples/example.android/albumtier/src/main/java/io/oz/albumtier/IFileProvider.java
+++ examples/example.android/albumtier/src/main/java/io/oz/albumtier/IFileProvider.java
@@ -7,21 +7,25 @@ import java.io.InputStream;
 import io.odysz.semantic.tier.docs.SyncDoc;
 
 /**
- * A file accessor used by AlbumTier etc., for accessing files without visiting traditional file system.
+ * <p>A file accessor used by AlbumTier etc., for accessing files without visiting traditional file system.</p>
  *
- * This is a special bridge that tiers can access file through Android content providers or so.
+ * This is a special bridge (interface) that semantiers can access file through Android content providers or so.
  */
 public interface IFileProvider {
     /**
-     * Default implementation example: return Files.size(Paths.get(f.fullpath());
+     * Default implementation example:<pre>
+     * return Files.size(Paths.get(f.fullpath());</pre>
+     * 
      * @return size
      * @throws IOException 
      */
     long meta(SyncDoc f) throws IOException;
 
     /**
-     * Example for normal file system implementation:
-     * <pre>new FileInputStream(new File(path));</pre>
+     * <p>Open file input stream.</p>
+     * Example for normal file system implementation:<pre>
+     * new FileInputStream(new File(path));</pre>
+     * 
      * @return readable stream
      */
     InputStream open(SyncDoc f) throws FileNotFoundException;
