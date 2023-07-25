79bbb7c9c47347fa4ad3cdb5ff7e9b1c7b65c0f0
odys zhou <odysseusj@163.com>
2023-07-25 19:23:12 +0800

    .

--- examples/example.js/album/webpack.config.js
+++ examples/example.js/album/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 
-var v = 'development'; // "production" | "development" | "none"
+var v = 'production'; // "production" | "development" | "none"
 var version = "0.1.5";
 
 module.exports = {
