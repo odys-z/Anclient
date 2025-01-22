fa271f5fcf3dbb8052c31b174b8f549750b761e5
Odys Zhou <odys.zhou@gmail.com>
2025-01-22 20:04:24 +0800

    .

--- examples/example.android/albumtier/src/test/java/io/oz/albumtier/AlbumContextTest.java
+++ examples/example.android/albumtier/src/test/java/io/oz/albumtier/AlbumContextTest.java
@@ -35,7 +35,7 @@ class AlbumContextTest {
 					try {
 						code[0] = 1;
 						AnsonBody reqbd = new DocsReq(uri)
-												.a(DocsReq.A.checkDev);
+										.a(DocsReq.A.checkDev);
 						AnsonMsg<AnsonBody> req = client.userReq(uri, AlbumPort.album, reqbd);
 						DocsResp resp = client.commit(req, (MsgCode c, String msg, String... args) -> {
 							fail(msg);
