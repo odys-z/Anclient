4ca91486805738f1ff2abc458f47befbdae1b448
Ody Z <odys.zhou@gmail.com>
2025-03-24 18:50:26 +0800

    Widget pdv-view is working:
    1. CSS transform for scale and translate canvase (the pdf rendering target);
    2. Using pdfjs-lagecy is compitable for Android 10 web browser (H30);
    3. The component, pdf-view, can be published as a React.js module if times allow.

--- js/anreact/src/react/widgets/pdf-view.tsx
+++ js/anreact/src/react/widgets/pdf-view.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Comprops, CrudCompW } from '../crud';
-import { GrabToPan } from '../../utils/grab_to_pan';
+import { CSSTransform } from '../../utils/css-transform';
 
 // Using legacy. To do: install types
 interface PDFDocumentLoadingTask {
@@ -15,11 +15,6 @@ interface PDFPageProxy {
   getViewport: (options: { scale: number }) => { width: number; height: number };
   render: (context: { canvasContext: CanvasRenderingContext2D; viewport: any }) => { promise: Promise<void> };
 }
-
-// export interface PdfObject {
-//   getPage: (p: number) => Promise<{ getViewport: (ref: { scale: number; }) => any;
-//   render : (ctx: { canvasContext: any; viewport: any; }) => void; }>;
-// }
 
 export interface PdfViewProps {
     /** Path to Mozzila PDF.js release, undefined for the CDN version. */
@@ -38,92 +33,57 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
     iconSize: 5,
   };
 
-  state = {
-    currentPage: 0,
-
-    // pageRendering: false,
-    pageNumPending: -1,
-  }
+  state = { }
 
   viewConfig = {
     currentPage: 0,
 
-    scale: 1.0,
-    touchDist: 0,
-
-    // script: HTMLScriptElement;
     canvas: undefined as HTMLCanvasElement,
     context: undefined,
 
     pdfdoc: undefined,
-    page: undefined as PDFPageProxy
-  }
+    page: undefined as PDFPageProxy,
 
+    transform: undefined as CSSTransform
+  }
 
   constructor (props: PdfViewProps) {
     super(props);
 
-    let canvas = document.getElementById('pdf') as HTMLCanvasElement;
-    let context = canvas.getContext('2d');
-
-    this.viewConfig = {currentPage: 0, scale: 1.0, touchDist: 0, canvas, context, pdfdoc: undefined, page: undefined};
-
-    let that = this;
-    canvas.addEventListener('wheel', (e) => {
-      e.preventDefault();
-
-      // updateZoom(scale);
-      const zoomFactor = e.deltaY > 0 ? 0.9 : 1.1; // Zoom out/in
-      let scale = Math.max(0.1, Math.min(5, this.viewConfig.scale * zoomFactor)); // Clamp between 0.1 and 5
-      canvas.style.transform = `scale(${scale / that.viewConfig.scale})`;
-      // Re-render if scale deviates too far (e.g., every 0.5 step)
-      if (Math.abs(scale - that.viewConfig.scale) > 0.5) {
-        that.viewConfig.scale = scale;
-      }
-
-      that.renderpdf(that.viewConfig);
-    });
-
-    canvas.addEventListener('touchmove', (e) => {
-      if (e.touches.length >= 2) {
-        e.preventDefault();
-        const touch1 = e.touches[0];
-        const touch2 = e.touches[1];
-        this.viewConfig.touchDist = Math.hypot(touch1.pageX - touch2.pageX, touch1.pageY - touch2.pageY);
-      }
-    });
-
-    canvas.addEventListener('mousemove', (e) => {
-      let grab = new GrabToPan({element: canvas});
-      grab.onMouseMove(e);
-    });
+    this.renderPage.bind(this);
+    this.hasprev.bind(this);
+    this.prevpage.bind(this);
+    this.hasnext.bind(this);
+    this.nextpage.bind(this);
   }
 
-  renderpdf = (cfg: {
-    currentPage: number;
-    context: CanvasRenderingContext2D;
-    page: PDFPageProxy;
-    canvas: any;
-  }) => {
+  renderPage() {
+    let cfg = this.viewConfig;
+    console.log(`Rendering page ${cfg.currentPage}`);
 
-    console.log('Page loaded');
+    cfg.pdfdoc.getPage(cfg.currentPage + 1).then((page) => {
+      cfg.page = page;
 
-    let scale = 1.0;
-    let viewport = cfg.page.getViewport({scale: scale});
+      let viewport = cfg.page.getViewport({scale: cfg.transform.scale0});
 
-    // Prepare canvas using PDF page dimensions
-    cfg.canvas.height = viewport.height;
-    cfg.canvas.width = viewport.width;
+      cfg.canvas.height = viewport.height;
+      cfg.canvas.width = viewport.width;
 
-    // Render PDF page into canvas context
-    let renderContext = {
-      canvasContext: cfg.context,
-      viewport
-    };
+      let renderContext = {
+        canvasContext: cfg.context,
+        viewport
+      };
 
-    let renderTask = cfg.page.render(renderContext);
-      renderTask.promise.then(function () {
-      console.log('Page rendered');
+      try {
+        let renderTask = cfg.page.render(renderContext);
+        let that = this;
+        renderTask.promise.then(function () {
+          that.setState({});
+        });
+      } catch (e) {
+        // Too quick to rerender
+        console.log(e);
+      }
     });
   }
 
@@ -140,12 +100,77 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
 
     let pdflink = this.props.src;
 
+    let screen = document.getElementById('screen') as HTMLCanvasElement;
+    let canvas = document.getElementById('pdf') as HTMLCanvasElement;
+    let context = canvas.getContext('2d');
+
+    this.viewConfig = {currentPage: 0, transform: new CSSTransform, canvas, context, pdfdoc: undefined, page: undefined};
+
+    canvas.addEventListener('wheel', (e) => {
+      e.preventDefault();
+
+      // Re-render if scale deviates too far (e.g., every 0.5 step)
+      if (this.viewConfig.transform.scaleBy(e) > 0.5) {
+        that.viewConfig.transform.stepScale();
+        that.renderPage();
+      }
+
+      canvas.style.transform = that.viewConfig.transform.transform();
+    });
+
+    canvas.addEventListener('touchend', (e) => {
+      console.log('touchend');
+      that.viewConfig.transform.end();
+      canvas.style.transform = that.viewConfig.transform.transform();
+    });
+
+    screen.addEventListener('touchend', (e) => {
+      that.viewConfig.transform.end();
+    });
+
+    canvas.addEventListener('touchstart', (e) => {
+      that.viewConfig.transform.end();
+      canvas.style.transform = that.viewConfig.transform.transform();
+    });
+
+    canvas.addEventListener('touchmove', (e) => {
+      that.viewConfig.transform.scaleTo(e);
+      that.viewConfig.transform.moveTo(e.touches[0]);
+      canvas.style.transform = that.viewConfig.transform.transform();
+    });
+
+    canvas.addEventListener('mouseup', (e) => {
+      that.viewConfig.transform.end();
+      canvas.style.transform = that.viewConfig.transform.transform();
+    });
+
+    screen.addEventListener('mouseup', (e) => {
+      that.viewConfig.transform.end();
+    });
+
+    canvas.addEventListener('mousedown', (e) => {
+      that.viewConfig.transform.start(e);
+    });
+
+    canvas.addEventListener('mousemove', (e) => {
+      let dxy = that.viewConfig.transform.moveTo(e);
+      if (dxy > 0) {
+        canvas.style.transform = that.viewConfig.transform.transform();
+      }
+    });
+
+    canvas.addEventListener('dblclick', (e) => {
+      that.viewConfig.transform.reset();
+      canvas.style.transform = that.viewConfig.transform.transform();
+      that.renderPage();
+    })
+
     // Run code after the script is loaded
     script.onload = () => {
       console.log('pdf.mjs loaded!');
 
       // let url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
-      let url = pdflink; //'private/CDSFL.pdf'; 
+      // let url = pdflink; //'private/CDSFL.pdf'; 
 
       // Loaded via <script> tag, create shortcut to access PDF.js exports.
       let { pdfjsLib } = globalThis as any;
@@ -156,18 +181,21 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
 
 
       // Asynchronous download of PDF
-      let loadingTask: PDFDocumentLoadingTask = pdfjsLib.getDocument(url) as PDFDocumentLoadingTask;
+      let loadingTask: PDFDocumentLoadingTask = pdfjsLib.getDocument(pdflink) as PDFDocumentLoadingTask;
       loadingTask.promise.then(function(pdf) {
         console.log('PDF loaded', pdf);
         
         that.viewConfig.pdfdoc = pdf;
 
         // Fetch the first page
-        let pageNumber = 1;
-        pdf.getPage(pageNumber).then(function(page) {
-          that.viewConfig.page = page;
-          that.renderpdf(that.viewConfig);
-        });
+        that.viewConfig.currentPage = 0;
+        that.viewConfig.pdfdoc = pdf;
+        that.renderPage();
+        // pdf.getPage(pageNumber).then(function(page) {
+        //   that.viewConfig.page = page;
+        //   that.renderPage();
+        // });
+
         }, function (reason) {
           // PDF loading error
           console.error(reason);
@@ -193,8 +221,32 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
     // }
   }
 
+  hasprev() {
+    return this.viewConfig.pdfdoc && this.viewConfig.pdfdoc.numPages > 0 && this.viewConfig.currentPage > 0;
+  }
+
+  prevpage() {
+    if (!this.hasprev())
+      return;
+
+    this.viewConfig.currentPage--;
+    this.renderPage();
+  }
+
+  hasnext() {
+    return this.viewConfig.pdfdoc && this.viewConfig.pdfdoc.numPages > 0 && this.viewConfig.currentPage < this.viewConfig.pdfdoc.numPages - 1;
+  }
+
+  nextpage() {
+    if (!this.hasnext())
+      return;
+
+    this.viewConfig.currentPage++;
+    this.renderPage();
+  }
+
   render() {
-    return (<div
+    return (<div id="screen"
     style={{
       top: '0px', left: '0px',
       overflow: 'hidden', position: 'fixed', display: 'flex',
@@ -217,36 +269,36 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
         </svg>
       </div>
-      {/* <div
+      {this.hasprev() && <div
         style={{
           background: '#cacaca77',
           borderRadius: '8px',
           position: 'absolute', left: '0px',
           zIndex: 1, cursor: 'pointer',
           color: '#FFFFFF',
-          fontSize: `${this.config.iconSize}px`
+          fontSize: `${this.uiConfig.iconSize}px`
         }}
         onClick={() => { this.prevpage(); }}>
         <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFFFFF">
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
         </svg>
-      </div>
-      <div
+      </div>}
+      {this.hasnext() && <div
         style={{
           background: '#cacaca77',
           borderRadius: '8px',
           position: 'absolute',
           right: '0px', zIndex: 1,
           color: '#FFFFFF', cursor: 'pointer',
-          fontSize: `${this.config.iconSize}px`
+          fontSize: `${this.uiConfig.iconSize}px`
         }}
         onClick={() => { this.nextpage(); }}>
         <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFFFFF">
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
         </svg>
-      </div> */}
+      </div>}
     </div>);
   }
 
---
+++ js/anreact/src/utils/css-transform.ts
@@ -0,0 +1,91 @@
+
+// TODO add a test case for this
+export class CSSTransform {
+
+    /** X, y translate */
+    txy: number[];
+
+    /** Client xy0 */
+    cxy: number[];
+
+    /** X, y delta */
+    dxy: number[];
+
+    /** Pinch Distances, [0]: starting, [1]: updating */
+    pds: number[];
+
+    scale : number;
+    scale0: number;
+
+    handling: boolean;
+
+    constructor() {
+        this.reset();
+
+        this.transform.bind(this);
+    }
+
+    reset() {
+        this.txy = [0, 0];
+        this.cxy = [0, 0];
+        this.dxy = [0, 0];
+        this.pds = undefined;
+        this.scale = 1.0;
+        this.scale0 = 1.0;
+        this.handling = false;
+    }
+
+    moveTo(e: {clientX: number, clientY: number}) {
+        if (this.handling) {
+            this.dxy[0] = e.clientX - this.cxy[0];
+            this.dxy[1] = e.clientY - this.cxy[1];
+            return Math.hypot(this.dxy[0], this.dxy[1]);
+        }
+        return 0;
+    }
+
+    start(e: {clientX: number, clientY: number}) {
+        this.handling = true;
+        this.handling = true;
+        this.cxy[0] = e.clientX;
+        this.cxy[1] = e.clientY;
+        this.dxy = [0, 0];
+        this.pds = undefined;
+    }
+
+    end() {
+        this.handling = false;
+        this.txy[0] += this.dxy[0];
+        this.txy[1] += this.dxy[1];
+        this.dxy = [0, 0];
+    }
+
+    scaleTo(e: {touches: TouchList}) {
+        if (this.handling)
+        if (e.touches && e.touches.length >= 2) {
+            let d = Math.hypot(e.touches[0].pageX - e.touches[1].pageX, e.touches[0].pageY - e.touches[1].pageY);
+            if (this.pds === undefined)
+                this.pds = [d, d];
+            else this.pds[1] = d;
+        }
+    }
+
+    scaleBy(e: {deltaY: number}) {
+        const zoomFactor = e.deltaY > 0 ? 0.9 : 1.1; // Zoom out/in
+        let scale = Math.max(0.1, Math.min(5, this.scale * zoomFactor)); // Clamp between 0.1 and 5
+        this.scale = scale;
+        return Math.abs(scale - this.scale0); 
+    }
+
+    stepScale() {
+        this.scale0 = this.scale;
+        return this;
+    }
+
+    transform() {
+        let s = this.scale / this.scale0;
+        let tx = (this.dxy[0] + this.txy[0]);
+        let ty = (this.dxy[1] + this.txy[1]);
+        return `scale(${s}) translate(${tx}px, ${ty}px)`;
+    }
+}
