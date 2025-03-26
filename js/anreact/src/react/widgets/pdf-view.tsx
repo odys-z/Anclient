0316f9eaec7a62134e17251b71f2740fde8492fd
Odys Zhou <odys.zhou@gmail.com>
2025-03-26 17:31:09 +0800

    fuck CCP -  I  [INFO:CONSOLE(5529)] "Warning: loadFont - translateFont failed: "UnknownErrorException: Unable to load binary CMap at: https://cdn.jsdelivr.net/npm/pdfjs-dist@5.0.375/cmaps/UniGB-UCS2-H.bcmap".", source: /pdfjs-legacy/pdf.worker.mjs (5529)

--- js/anreact/src/react/widgets/pdf-view.tsx
+++ js/anreact/src/react/widgets/pdf-view.tsx
@@ -198,7 +198,10 @@ export class PdfView extends CrudCompW<Comprops & PdfViewProps> {
       // Asynchronous download of PDF
       // let loadingTask: PDFDocumentLoadingTask = pdfjsLib.getDocument(pdflink) as PDFDocumentLoadingTask;
       let loadingTask: PDFDocumentLoadingTask = pdfjsLib.getDocument({url: pdflink,
-        cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@5.0.375/cmaps/'
+        // fuck CCP
+        // I  [INFO:CONSOLE(5529)] "Warning: loadFont - translateFont failed: "UnknownErrorException: Unable to load binary CMap at: https://cdn.jsdelivr.net/npm/pdfjs-dist@5.0.375/cmaps/UniGB-UCS2-H.bcmap".", source: /pdfjs-legacy/pdf.worker.mjs (5529)
+        // cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@5.0.375/cmaps/'
+        cMapUrl: ''
       }) as PDFDocumentLoadingTask;
 
       loadingTask.promise.then(function(pdf) {
