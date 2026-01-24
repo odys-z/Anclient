8a445e95c2a4cb6b89caafbd177c405c96a527bc
Odys Zhou <odys.zhou@gmail.com>
2026-01-24 23:37:55 +0800

    cmake compiled: use rttr master

--- examples/example.qml/src/clients.cpp
+++ examples/example.qml/src/clients.cpp
@@ -1,9 +1,4 @@
 #include "clients.h"
 
 namespace anson {
-
-// AnsonResp& InsecureClient::commit_ws(AnsonResp & resp) {
-//     return resp;
-// }
-
 }
--- examples/example.qml/src/clients.h
+++ examples/example.qml/src/clients.h
@@ -6,19 +6,4 @@
 
 using namespace std;
 namespace anson {
-
-// class InsecureClient {
-// public:
-//     AnsonResp& commit_ws(AnsonResp &resp);
-// };
-
-// class Clients {
-// public:
-//     static AnsonResp ping_less(string funcuri, OnError e) {
-//         InsecureClient client;
-//         AnsonResp resp;
-//         return client.commit_ws(resp);
-//     }
-// };
-
 }
